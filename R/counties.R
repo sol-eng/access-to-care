425e93963ab484fe5901d041c44405290801af9b
Edgar Ruiz <edgararuiz@gmail.com>
2019-03-26 20:05:24 -0500

    Handles multiple shapes for a county

--- R/counties.R
+++ R/counties.R
@@ -23,27 +23,26 @@ imap(
 extract_coordinates <- function(path, decimals = 2) {
   state_rds <- readRDS(path)
   state <- state_rds@polygons[[2]]@Polygons[[1]]@coords
-  map(
-    state_rds@polygons,
-    ~.x@Polygons[[1]]@coords
-  ) %>%
+  state_rds@polygons %>%
+    map(~ {
+      map(
+        .x@Polygons,
+        ~ tibble(
+          long = round(.x@coords[, 1], decimals),
+          lat = round(.x@coords[, 2], decimals),
+          rn = 1:length(.x@coords[, 1]))) %>% 
+        set_names(paste0("s", seq_along(.x@Polygons))) %>%
+        map_df(~.x, .id = "shape_id")}) %>%
     set_names(state_rds$NAME) %>%
-    imap_dfr(
-      ~ tibble(
-        county = .y,
-        long = round(.x[, 1], decimals),
-        lat = round(.x[, 2], decimals),
-        rn = 1:length(.x[, 1])
-      )
-    ) %>%
-    group_by(county, long, lat) %>%
+    map_df(~.x, .id = "county") %>%
+    group_by(county, shape_id, long, lat) %>%
     summarise(rn = max(rn)) %>%
     ungroup() %>% 
-    arrange(county, rn) %>%
-    group_by(county) %>%
+    arrange(county, shape_id, rn) %>%
+    group_by(county, shape_id) %>%
     mutate(point_no = row_number()) %>%
     ungroup() %>%
-    select(county, long, lat, point_no)
+    select(county, shape_id, long, lat, point_no) 
 }
 
 county_states <- map_dfr(
