e9d1873e1583ca2699a3fee05f328389f00d7d91
Edgar Ruiz <edgar@rstudio.com>
2019-11-23 19:43:56 -0600

    Adds manifest

---
+++ RMarkdown-html/rs-connect-git.R
@@ -0,0 +1,5 @@
+rsconnect::writeManifest(
+  appDir = file.path(here::here(), "RMarkdown-html"),
+  appFiles = c("access-to-care-email.Rmd", "access-to-care-rmarkdown.Rmd", "RStudio1.png"),
+  appPrimaryDoc = "access-to-care-rmarkdown.Rmd", 
+)
