6f2538c203d7b401849884b311455d5948401f11
Edgar Ruiz <edgar@rstudio.com>
2019-12-04 12:32:11 -0600

    Updates presentations, and adds git backed scripts to most

--- RMarkdown-pdf/rs-connect-git.R
+++ RMarkdown-pdf/rs-connect-git.R
@@ -1,11 +1,13 @@
-sub_folder <- "RMarkdown-html"
+sub_folder <- "RMarkdown-pdf"
 full_path <- here::here(sub_folder)
 app_files <- dir(full_path)
 app_files <- app_files[app_files != "config.yml"]
 app_files <- app_files[!grepl(".html", app_files)]
 app_files <- app_files[!grepl(".pptx", app_files)]
+app_files <- app_files[!grepl(".pdf.pdf", app_files)]
 primary_id <- grepl(".Rmd", app_files)
-primary_doc <- "access-to-care-rmarkdown.Rmd"
+if(sum(primary_id) > 1) stop("There are more then one Rmd document, please identify the proper primary doc")
+primary_doc <- app_files[primary_id]
 rsconnect::writeManifest(
   appDir = full_path,
   appFiles = app_files,
--- jupyter/rs-connect-git.R
+++ jupyter/rs-connect-git.R
@@ -1,8 +1,7 @@
-sub_folder <- "plumber-api"
+sub_folder <- "jupyter"
 full_path <- here::here(sub_folder)
 app_files <- dir(full_path)
-primary_id <- grepl("plumber.R", app_files)
-if(sum(primary_id) > 1) stop("There are more then one Rmd document, please identify the proper primary doc")
+primary_id <- grepl("access-to-care.ipynb", app_files)
 primary_doc <- app_files[primary_id]
 rsconnect::writeManifest(
   appDir = full_path,
