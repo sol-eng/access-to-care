4cdb2bf103a8a1371b2774cfb25327f7bd6d05a8
Edgar Ruiz <edgar@rstudio.com>
2019-12-12 09:40:20 -0600

    Updates plumber app

--- plumber-api/plumber.R
+++ plumber-api/plumber.R
@@ -14,46 +14,31 @@ if(Sys.getenv("R_CONFIG_ACTIVE") == "rsconnect") {
   board_register(boardname)  
 }
 
-hospitals <- pin_get("atc-county_hospitals", board = boardname) %>%
-  mutate(state = as.character(state)) %>%
-  inner_join(statepop, by = c("state" = "abbr")) 
+states <-pin_get("atc-states", board = boardname)  
+counties <- pin_get("atc-base-map", boardname)
+model <- pin_get("atc-model", board = boardname) 
 
 #* Get the state's summarized information
 #* @get /summary
-#* @param state Two letter abbriviation of the state
+#* @param state Two letter abbreviation of the state
 function(state = "MS") {
   st_name <- toupper(state)
-  state_data <- hospitals[hospitals$state == st_name, ]
-  data.frame(
-    population = sum(state_data$population),
-    hospitals = sum(state_data$hospitals),
-    counties = nrow(state_data),
-    underserved = nrow(state_data[state_data$result == -1, ])
-  )
+  state_data <- states[states$state == st_name, ]
+  state_data
 }
 
-#* Prediction of number of hospitals based on population
+#* Prediction of number of hospitals based on a provided population number
 #* @get /model
 #* @param population:numeric Number of people living in a given county
 function(population = 70000) {
-  model <- pin_get("atc-model", board = boardname) 
   pop <- as.numeric(population)
   predict(model, data.frame(population = pop))
 }
 
 #* Prediction of number of hospitals based on population
 #* @get /state
-#* @param state Two letter abbriviation of the state
+#* @param state Two letter abbreviation of the state
 function(state = "MS") {
   st_name <- toupper(state)
-  hospitals[hospitals$state == st_name, ]
-}
-
-#* Coordinates of hospitals for a given state
-#* @get /hospitals
-#* @param state Two letter abbriviation of the state
-function(state = "MS") {
-  hospital_list <- pin_get("atc-hospital-locations", board = boardname) 
-  st_name <- toupper(state)
-  hospital_list[hospital_list$state == st_name, 2:3]
+  counties[counties$state == st_name, ]
 }
