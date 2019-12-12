4f0fea759011586af8a8da98d5a99176d8973da6
edgararuiz <edgararuiz@gmail.com>
2019-12-12 19:18:35 +0000

    Updates jupyter notebook

--- jupyter/rs-connect-git.R
+++ jupyter/rs-connect-git.R
@@ -1,10 +1,10 @@
 sub_folder <- "jupyter"
 full_path <- here::here(sub_folder)
 app_files <- dir(full_path)
-primary_id <- grepl("access-to-care.ipynb", app_files)
+primary_id <- grepl("access-to-care.html", app_files)
 primary_doc <- app_files[primary_id]
 rsconnect::writeManifest(
   appDir = full_path,
-  appFiles = app_files,
+  appFiles = primary_doc,
   appPrimaryDoc = primary_doc
 )
