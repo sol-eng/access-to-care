acb8c3ae341a9f1bb95d74e224187ce35265fa05
Edgar Ruiz <edgar@rstudio.com>
2019-12-11 18:37:12 -0600

    removes category and manifest from list

--- flexdashboard/rs-connect-git.R
+++ flexdashboard/rs-connect-git.R
@@ -4,12 +4,12 @@ app_files <- dir(full_path)
 app_files <- app_files[app_files != "config.yml"]
 app_files <- app_files[app_files != "rs-connect-git.R"]
 app_files <- app_files[app_files != ".gitignore"]
+app_files <- app_files[app_files != "manifest.json"]
 primary_id <- grepl(".Rmd", app_files)
 if(sum(primary_id) > 1) stop("There are more then one Rmd document, please identify the proper primary doc")
 primary_doc <- app_files[primary_id]
 rsconnect::writeManifest(
   appDir = full_path,
   appFiles = app_files,
-  appPrimaryDoc = primary_doc,
-  contentCategory = "document"
+  appPrimaryDoc = primary_doc
 )
