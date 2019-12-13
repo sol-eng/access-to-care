f7a9e469a19b72b1ec70ae0422013217f45ee29e
Edgar Ruiz <edgar@rstudio.com>
2019-12-13 08:18:36 -0600

    Adds pop ups to leaflet

--- htmlwidgets/access-to-care-hospitals.R
+++ htmlwidgets/access-to-care-hospitals.R
@@ -3,28 +3,17 @@ library(dplyr)
 library(htmlwidgets)
 library(leaflet)
 
-under <- "#CC79A7"
-over <- "#0072B2"
-at_level <- "#008b00"
-hospital_color <- "#F0E442"
-
-if(Sys.getenv("R_CONFIG_ACTIVE") == "rsconnect") {
-  boardname <- "rsconnect"
-  board_register_rsconnect(
-    server = Sys.getenv("connect_url"),
-    key = Sys.getenv("connect_key")
-  )
-} else {
-  boardname <- "local"
-  board_register(boardname)  
-}
-
-hospitals <- pin_get("atc-hospitals", boardname)
+hospitals <- pin_get("atc-hospital-list", "local")
 
 hospital_maps <- hospitals %>%
   leaflet() %>%
   addTiles() %>%
-  addMarkers(~longitude, ~latitude, clusterOptions = markerClusterOptions())
+  addMarkers(
+    ~longitude, 
+    ~latitude, 
+    popup = ~ paste(paste0("<b>", hospital_name, "</b>"), address, sep = "<br/>"), 
+    clusterOptions = markerClusterOptions()
+    )
 
 saveWidget(hospital_maps, here::here("htmlwidgets/map.html"))
 
