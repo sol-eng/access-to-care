8297505fb5c24909de19dce39832ae5e872a24a6
Edgar Ruiz <edgararuiz@gmail.com>
2021-09-08 16:29:45 -0500

    Switches to shiny app for connectwidgets

--- connectwidgets/app.R
+++ connectwidgets/app.R
@@ -1,29 +1,100 @@
 library(shiny)
-library(shinymaterial)
 library(connectwidgets)
+library(shinymaterial)
 library(dplyr)
 library(stringr)
+library(fs)
+library(readr)
+library(accesstocare)
 
-connect_server <- Sys.getenv("CONNECT_SERVER")
-connect_key <- Sys.getenv("CONNECT_API_KEY")
 
-client <- connect(server  = connect_server, api_key = connect_key)
+if (Sys.getenv("R_CONFIG_ACTIVE") == "rsconnect") {
+  connect_server <- Sys.getenv("CONNECT_SERVER")
+  connect_key <- Sys.getenv("CONNECT_API_KEY")
+  client <- connect(server = connect_server, api_key = connect_key)
+  all_content <- content(client)
+} else {
+  if (!file_exists("content.rds")) {
+    connect_server <- Sys.getenv("CONNECT_SERVER")
+    connect_key <- Sys.getenv("CONNECT_API_KEY")
+    client <- connect(server = connect_server, api_key = connect_key)
+    all_content <- content(client)
+    write_rds(all_content, "content.rds")
+  } else {
+    all_content <- read_rds("content.rds")
+  }
+}
 
-# Pull data from RStudio Connect
-all_content <- content(client) 
-
-atc_content <- all_content %>% 
-  by_tags("Access to Care") %>% 
-  mutate(title = str_remove(title, "Access to Care - ")) 
-
-# Wrap shinymaterial apps in material_page
-ui <- material_page(
-  title = "Basic Page",
-  tags$h1("Page Content"),
-  rsccardOutput("content")
+choice_types <- c(
+  "All", "Script", "Report", "Dashboard", "Data", "Model",
+  "Notebook", "Plot", "Presentation"
 )
 
-server <- function(input, output) {
-  output$content <- renderRsccard(rsc_card(atc_content))
+atc_content <- all_content %>%
+  by_tags("Access to Care") %>%
+  mutate(
+    title = str_remove(title, "Access to Care - "),
+    type = case_when(
+      str_detect(title, "Jupyter|RNotebook") ~ "Notebook",
+      content_category == "pin" & str_detect(title, "Model") ~ "Model",
+      content_category == "pin" ~ "Data",
+      str_detect(title, " Prep") ~ "Script",
+      str_detect(title, "Presentation|PowerPoint") ~ "Presentation",
+      app_mode == "rmd-static" ~ "Report", 
+      app_mode %in% c("rmd-shiny", "python-dash") ~ "Dashboard",
+      app_mode == "static" ~ "Plot", 
+      TRUE ~ "Other"
+    ),
+    language = case_when(
+      str_detect(title, "Jupyter") ~ "python",
+      str_detect(app_mode, "python") ~ "python",
+      TRUE ~ "R"
+    )
+  )
+
+
+ui <- material_page(
+  title = "Access to Care",
+  primary_theme_color = palette_atc$ok,
+  secondary_theme_color = palette_atc$above,
+  background_color = "white",
+  material_parallax("hospital.jpeg"),
+  fluidRow(
+    absolutePanel(
+      material_radio_button("type", "Content Type", choice_types),
+      right = 80,
+      top = 70
+    )
+  ),
+  fluidRow(
+    absolutePanel(
+      material_radio_button("language", "Language", c("All", "R", "python")),
+      right = 300,
+      top = 70
+    )
+  ),
+  fluidRow(
+    absolutePanel(
+      rsccardOutput("cards", width = "90%"),
+      left = 200,
+      width = "80%"
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  output$cards <- renderRsccard({
+    fa <- atc_content
+
+    if (input$type != "All") fa <- filter(fa, type == input$type)
+
+    if (input$language != "All") fa <- filter(fa, language == input$language)
+
+    if (nrow(fa) > 0) {
+      rsc_card(fa)
+    } else {
+      NULL
+    }
+  })
 }
 shinyApp(ui = ui, server = server)
